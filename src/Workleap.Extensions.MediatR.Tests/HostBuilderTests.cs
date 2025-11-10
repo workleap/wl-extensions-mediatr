d17e6ed79c40e975802179206e15cf65528fb8b8
EsgAtWorkleap <176311182+EsgAtWorkleap@users.noreply.github.com>
2025-11-10 11:02:26 -0500

    [SSD-4123] Upgrade MediatR to 13.1 (#135)
    * [SSD-4123] Upgrade MediatR to 13.1
    
    ServiceCollectionExtensions was renamed to MediatRServiceCollectionExtensions
    
    * Fix SetLicenseFromConfiguration test
    
    * Support both 13 and 13.1

--- src/Workleap.Extensions.MediatR.Tests/HostBuilderTests.cs
+++ src/Workleap.Extensions.MediatR.Tests/HostBuilderTests.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Workleap.Extensions.MediatR.Tests;
 public sealed class HostBuilderTests
@@ -11,6 +12,12 @@ public sealed class HostBuilderTests
     public async Task SetLicenseFromConfiguration()
     {
         var builder = WebApplication.CreateBuilder();
+        builder.Host.UseDefaultServiceProvider(options =>
+        {
+            // Disabling build on validation since some private classes (like TestState) are not registered
+            options.ValidateOnBuild = false;
+        });
+
         builder.Services.AddMediator(typeof(HostBuilderTests).Assembly);
         builder.Configuration.AddInMemoryCollection([KeyValuePair.Create<string, string?>("MEDIATR_LICENSE_KEY", "License")]);
 
